c187c9b216fab757e5499c27e6e163e8722d5963
Jeremy Davis <jeremy2014@theppn.org>
2014-12-19 20:16:51 +0530

    normalise account number

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,9 @@
 class Account < ActiveRecord::Base
+  before_validation :normalise_account_number
   belongs_to :currency
+
+  private
+  def normalise_account_number
+    self.account_number = self.account_number.tr("^0-9", '')
+  end
 end
