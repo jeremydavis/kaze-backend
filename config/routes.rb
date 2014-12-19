5b78abd8c247644da92f1b2335e9780838ea3591
Jeremy Davis <jeremy2014@theppn.org>
2014-12-19 10:58:44 +0530

    clean routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'welcome/index'
 
+  get 'welcome/index'
   get 'welcome/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
