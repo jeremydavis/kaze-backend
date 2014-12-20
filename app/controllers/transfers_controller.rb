98022e1903f3b8c022e53dccc965b31048fd2c98
Jeremy Davis <jeremy2014@theppn.org>
2014-12-20 14:12:09 +0530

    Add transaction password validation to transfers

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -3,7 +3,15 @@ class TransfersController < InheritedResources::Base
   private
 
     def transfer_params
-      params.require(:transfer).permit(:account_id, :payee_id, :currency_id, :value, :description, :transfer_date, :repeat, :repeat_until)
+      params.require(:transfer).permit :account_id,
+                                       :payee_id,
+                                       :currency_id,
+                                       :value,
+                                       :description,
+                                       :transfer_date,
+                                       :repeat,
+                                       :repeat_until,
+                                       :transaction_password
     end
 end
 
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -3,9 +3,21 @@ class Transfer < ActiveRecord::Base
   belongs_to :payee
   belongs_to :currency
 
-  validates :account_id, :payee_id, :currency_id, :value, :description, :transfer_date, presence: true
+  attr_accessor :transaction_password
+
+  validates :account_id,
+            :payee_id,
+            :currency_id,
+            :value,
+            :description,
+            :transfer_date,
+            :transaction_password,
+            { presence: true }
   validate :transfer_date_is_in_the_future, if: :transfer_date
   validate :repeat_until_date_is_after_transfer_date, if: :repeat?
+  validate :confirm_transaction_password, if: :transaction_password
+
+  after_validation :clear_transaction_password, if: :transaction_password
 
   private
   def transfer_date_is_in_the_future
@@ -17,5 +29,18 @@ class Transfer < ActiveRecord::Base
     if !self.repeat_until || self.repeat_until <= (self.transfer_date >> 1)
       errors.add(:repeat_until, 'Repeat transfers must last for at least 1 month')
     end
+  end
+  def confirm_transaction_password
+    case self.transaction_password
+    when '1234'
+      errors.add(:transaction_password, 'Password Expired')
+    when '234wer'
+      self.transaction_password_valid = true
+    else
+      errors.add(:transaction_password, "Password Invalid #{self.transaction_password}")
+    end
+  end
+  def clear_transaction_password
+    self.transaction_password = nil
   end
 end
