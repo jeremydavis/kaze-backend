d95f4e7429fa2079b7cedf3fa2759588526bb1e2
Jeremy Davis <jeremy2014@theppn.org>
2015-01-11 21:08:44 +0530

    Correct devise route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :currencies
 
-  devise_for :users
+  devise_for :users, controllers: {sessions: "sessions"}
 
   get 'welcome/index'
   get 'welcome/help'
